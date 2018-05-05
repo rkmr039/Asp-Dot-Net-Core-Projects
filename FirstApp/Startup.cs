6feccdc09ae79d0fe8344214ae3f41f0e656afe7
Rishab Kumar <rkmr039@gmail.com>
2018-05-05 18:07:18 +0530

    Add files via upload

--- FirstApp/Startup.cs
+++ FirstApp/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FirstApp
@@ -18,13 +19,19 @@ namespace FirstApp
         {
             //services like ADDMvc, AddDbContext. etc
             // it supports the concept of Dependency Injection
-        }
+        } 
 
         // configure is a middleware, a pipeline for HTTP request and response.
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
+            defaultFilesOptions.DefaultFileNames.Clear();
+            defaultFilesOptions.DefaultFileNames.Add("home.html");
+            app.UseDefaultFiles(defaultFilesOptions);
             app.UseStaticFiles();//http://localhost:61983/home.html
+
+            
         }
     }
 }
